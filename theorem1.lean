e1f2a8d4a76d98de7a6933646fab65bce1a6f068
FriendChicken <78368334+FriendChicken@users.noreply.github.com>
2025-06-15 18:53:34 +0100

    change theorem1 back to origin

--- theorem1.lean
+++ theorem1.lean
@@ -10,47 +10,26 @@ import Mathlib.LinearAlgebra.Dimension.Finrank
 import Mathlib.LinearAlgebra.Dimension.Constructions
 import Mathlib.Data.Fintype.Defs
 
+
+
 open Finset
 variable {α : Type} (n : ℕ) [DecidableEq α]
 variable {X: Finset α} (F: Finset X.powerset)
 
-
-
-
-
-
-
-
-
-
-
 def uniform {X: Finset α} (F: Finset X.powerset) (k : ℕ) : Prop := ∀ (A : F), #A.val.val = k
-
-
-
-
 
 def intersecting {X: Finset α} (F: Finset X.powerset) (L : Set ℕ) :=
   ∀ (A B: F), #(A.val.val ∩ B.val.val) ∈ L
 
-
-
 variable (k s: ℕ) (L : Finset ℕ)
 
 
-instance: Module ℝ (F → ℝ) := sorry
+instance: Module ℝ (F → ℝ) := by infer_instance
 
-
-instance: Module ℝ (F → ℝ) := sorry
-
-
-
-
-def S_bar (S : Finset α): F → ℝ  :=
-def S_bar (S : Finset α): F → ℝ  :=
+/--The indicator vector of subset S: S = ∑(A: A ∈ F, S ⊆ A).-/
+def subset_indicator (S : Finset α): F → ℝ  :=
     fun A => if S ⊆ A then 1 else 0
 
-<<<<<<< HEAD
 /--The intersection indicator vector of subset S: H = ∑(B:B ∈ F,|B∩S|=μ)-/
 def intersection_indicator (S: Finset α) (μ : ℕ): F → ℝ :=
     fun B => if #(B ∩ S) = μ then 1 else 0
@@ -62,7 +41,7 @@ def subset_intersection_indicator (A: Finset α) (μ : ℕ): F → ℝ :=
 variable (r: ℕ)
 
 --TODO
-lemma indicator_eq (A: Finset α): subset_intersection_indicator F s A r =
+lemma indicator_eq: subset_intersection_indicator F s A r =
     ∑ (l: L), (Nat.choose l r) * (Nat.choose (#A - l) (s - r))
     * (intersection_indicator F A l) := by
   unfold subset_intersection_indicator
@@ -80,54 +59,30 @@ lemma indicator_eq (A: Finset α): subset_intersection_indicator F s A r =
   rw [h1]
   sorry
 
-=======
-example (f: ℕ →  ℕ ) (h : (a:ℕ) = b) : f a =  f b := by exact congrArg f h
->>>>>>> parent of 030fdbe (added G_r)
 
 variable (S: X.powerset)
-theorem span_bar: Submodule.span ℝ { f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s}
-    = (⊤: Submodule ℝ (F → ℝ)) := sorry
-theorem span_bar: Submodule.span ℝ { f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s}
-    = (⊤: Submodule ℝ (F → ℝ)) := sorry
 
-#check { x // x ∈ X.powerset }
-#check { x // x ∈ X.powerset }
 
-theorem finrank_R {β: Type} (ι : Finset β):
-    Module.finrank ℝ (ι → ℝ) = Fintype.card ι := by
-  simp [Module.finrank]
-theorem finrank_R {β: Type} (ι : Finset β):
+/--The set of indicator vectors {S_bar : S ∈ 𝓟ₛ(X)}-/
+noncomputable def subset_indicator_set :=
+  Finset.image (fun (S : Finset α) => (subset_indicator F S: F → ℝ)) (powersetCard s X)
+
+
+theorem my_finrank_pi (ι : Type) [Fintype ι]:
     Module.finrank ℝ (ι → ℝ) = Fintype.card ι := by
   simp [Module.finrank]
 
-theorem finrank_pi (ι : Type) [Fintype ι]:
-theorem finrank_pi (ι : Type) [Fintype ι]:
-    Module.finrank ℝ (ι → ℝ) = Fintype.card ι := by
-  simp [Module.finrank]
 
-lemma F_rank : Module.finrank ℝ (⊤: Submodule ℝ (F → ℝ)) = #F := by
-lemma F_rank : Module.finrank ℝ (⊤: Submodule ℝ (F → ℝ)) = #F := by
+lemma F_rank {α : Type} {X : Finset α} (F : Finset { x // x ∈ X.powerset }):
+    Module.finrank ℝ (⊤: Submodule ℝ (F → ℝ)) = #F := by
   simp only [finrank_top]
-  have h:= finrank_pi { x // x ∈ F }
-  have h:= finrank_pi { x // x ∈ F }
   rw [← Fintype.card_coe F]
-  exact h
+  exact my_finrank_pi F
 
 
-noncomputable def S_bar_set :=
-  Finset.image (fun (S : Finset α) => (S_bar F S: F → ℝ)) (powersetCard s X)
-  exact h
-
-
-noncomputable def S_bar_set :=
-  Finset.image (fun (S : Finset α) => (S_bar F S: F → ℝ)) (powersetCard s X)
-
-
-lemma S_bar_rank (hX : #X = n): #(S_bar_set F s)
-lemma S_bar_rank (hX : #X = n): #(S_bar_set F s)
+lemma subset_indicator_rank (hX : #X = n): #(subset_indicator_set F s)
     ≤ Nat.choose n s := by
-  have h1 : #(S_bar_set F s) ≤ #(powersetCard s X) := by
-  have h1 : #(S_bar_set F s) ≤ #(powersetCard s X) := by
+  have h1 : #(subset_indicator_set F s) ≤ #(powersetCard s X) := by
     exact Finset.card_image_le
   have h2 : #(powersetCard s X) = n.choose s := by
     have h := (Finset.card_powersetCard s X).symm
@@ -136,42 +91,29 @@ lemma S_bar_rank (hX : #X = n): #(S_bar_set F s)
   rw [h2.symm]
   exact h1
 
+#check rank_span_finset_le
 
 
-example (h: Submodule.span ℝ (S_bar_set F s) = (⊤: Submodule ℝ (F → ℝ))):
-    #F ≤ Nat.choose n s := by
-  have h1 : Module.finrank ℝ (Submodule.span ℝ { f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s})
-
-
-example (h: Submodule.span ℝ (S_bar_set F s) = (⊤: Submodule ℝ (F → ℝ))):
-    #F ≤ Nat.choose n s := by
-  have h1 : Module.finrank ℝ (Submodule.span ℝ { f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s})
+lemma subset_vector_span_dim_le (h: Submodule.span ℝ (toSet (subset_indicator_set F s)) = (⊤: Submodule ℝ (F → ℝ)))
+  (hX : #X = n) : #F ≤ Nat.choose n s := by
+  have h1 : Module.finrank ℝ (Submodule.span ℝ (toSet (subset_indicator_set F s)))
       = Module.finrank ℝ (⊤: Submodule ℝ (F → ℝ)) := by
     rw [h]
   rw [F_rank] at h1
-  have hF : Fintype F := sorry
-  have h2 := S_bar_rank n { x // x ∈ powerset X} s
-  have h3 : Module.finrank ℝ (Submodule.span ℝ
-      { f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s})
-      ≤ #{ f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s} := sorry
-  have hF : Fintype F := sorry
-  have h2 := S_bar_rank n { x // x ∈ powerset X} s
-  have h3 : Module.finrank ℝ (Submodule.span ℝ
-      { f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s})
-      ≤ #{ f | ∃ (S : X.powerset), f = S_bar F S ∧ #S.val = s} := sorry
+  have h2 := subset_indicator_rank n F s hX
+  have h3 : Module.finrank ℝ (Submodule.span ℝ (toSet (subset_indicator_set F s)))
+      ≤ #(subset_indicator_set F s) := by
+    have h : Module.rank ℝ (Submodule.span ℝ (toSet (subset_indicator_set F s)))
+      ≤ #(subset_indicator_set F s) := by
+        exact rank_span_finset_le (subset_indicator_set F s)
+    exact Module.finrank_le_of_rank_le h
   rw [h1] at h3
   exact Nat.le_trans h3 h2
 
-  done
-  done
+
+theorem span_bar: Submodule.span ℝ (subset_indicator_set F s)
+    = (⊤: Submodule ℝ (F → ℝ)) := sorry
+
 
 theorem Ray_Chaudhuri_Wilson (huniform: uniform F k) (hintersect : intersecting F L)
     (hL : #L = s): #F ≤ Nat.choose n s := by sorry
-
-
-
-def vector_space_F (F: Finset X.powerset) := F → ℝ
-
-
-
-def vector_space_F (F: Finset X.powerset) := F → ℝ
